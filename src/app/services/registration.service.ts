d6835392d7762901fdb03ad0303ba7517de21aeb
Prajwal Makanwar <prajwal.makanwar@gmail.com>
2021-03-24 12:35:54 +0530

    Components Updated.

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -18,20 +18,20 @@ export class RegistrationService {
   constructor(private _http: HttpClient) { }
 
   public loginUserFromRemote(user:User):Observable<any>{
-    return this._http.post<any>("http://localhost:8888/login",user);
+    return this._http.post<any>("http://localhost:8080/login",user);
   }
 
   public registerUserFromRemote(user:User):Observable<any>{
-    return this._http.post<any>("http://localhost:8888/registeruser",user);
+    return this._http.post<any>("http://localhost:8080/registeruser",user);
   }
 
   public registerVaccineAppointment(v:Vaccine):Observable<any>{
-    return this._http.post<any>("http://localhost:8888/vaccination",v);
+    return this._http.post<any>("http://localhost:8080/vaccination",v);
   }
 
 
   public confirmAppointment():Observable<any>{
-    return this._http.get<any>("http://localhost:8888/vaccination");
+    return this._http.get<any>("http://localhost:8080/vaccination");
   }
 
   public generalAppointmentFromRemote(general:General):Observable<any>{
@@ -41,7 +41,5 @@ export class RegistrationService {
   hide() { this.visible = false; }
 
   show() { this.visible = true; }
-
-  
 
 }
